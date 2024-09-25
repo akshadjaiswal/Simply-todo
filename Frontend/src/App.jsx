179d5cdb4aa96a66dff86ee64060694bd0ae5656
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-09-25 21:20:52 +0530

    Feature: Create and Display Components ⚙️

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { CreateTodo } from './Components/CreateTodo'
+import { Todos } from './Components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <CreateTodo></CreateTodo>
+        <Todos></Todos>
 
       </div>
 
---
+++ Frontend/src/Components/Todos.jsx
@@ -0,0 +1,9 @@
+export function Todos() {
+    return (
+        <div>
+            <h1>Gym</h1>
+            <h2>You need to go to the Gym</h2>
+            <button>Mark as Completed</button>
+        </div>
+    )
+}
