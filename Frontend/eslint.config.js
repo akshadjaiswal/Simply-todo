56c6f0718ac0f032899864c6efe76b28592edb72
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-07 23:43:49 +0530

    Temporary Comment

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -23,18 +23,18 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": "off",
-      "react/prop-types": "off",
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        // { allowConstantExport: true },
-      ],
-    },
+    // rules: {
+    //   ...js.configs.recommended.rules,
+    //   ...react.configs.recommended.rules,
+    //   ...react.configs['jsx-runtime'].rules,
+    //   ...reactHooks.configs.recommended.rules,
+    //   "no-unused-vars": "off",
+    //   "react/prop-types": "off",
+    //   'react/jsx-no-target-blank': 'off',
+    //   'react-refresh/only-export-components': [
+    //     'warn',
+    //     // { allowConstantExport: true },
+    //   ],
+    // },
   },
 ]
